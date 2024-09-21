dd0acb633f4a9ab3a799814f891b8653c67ba7cc
Magnus <magnus.nistad.helvig@gmail.com>
2024-09-21 14:51:47 +0200

    Working Menu

---
+++ Assets/Scripts/Menu/MainMenuController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenuController : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit()
+#endif
+
+    }
+}
